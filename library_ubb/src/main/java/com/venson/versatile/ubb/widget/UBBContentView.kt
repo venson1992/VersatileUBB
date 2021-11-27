9853d8ddee7e922319f6aff972c5ce50ce994380
Venson <a3131312@qq.com>
2021-11-27 17:11:11 +0800

    1、自定义属性

--- library_ubb/src/main/java/com/venson/versatile/ubb/widget/UBBContentView.kt
+++ library_ubb/src/main/java/com/venson/versatile/ubb/widget/UBBContentView.kt
@@ -1,48 +1,131 @@
 package com.venson.versatile.ubb.widget
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Build
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import android.widget.LinearLayout
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
 import androidx.annotation.RequiresApi
+import com.venson.versatile.ubb.R
 import com.venson.versatile.ubb.UBB
 import com.venson.versatile.ubb.adapter.UBBContentAdapter
 import com.venson.versatile.ubb.bean.UBBContentBean
 import com.venson.versatile.ubb.bean.ViewHolderType
 import com.venson.versatile.ubb.convert.UBBContentViewConvert
 import com.venson.versatile.ubb.holder.DefaultViewHolder
+import com.venson.versatile.ubb.utils.TypedArrayUtils
 import kotlinx.coroutines.*
+import kotlin.math.roundToInt
 
 /**
  * UBB内容展示
  */
 class UBBContentView : LinearLayout {
 
+    companion object {
+        private const val IMAGE_WIDTH_MATCH = -1
+        private const val IMAGE_WIDTH_WRAP = -2
+    }
+
+    @ColorInt
+    private var mTextColor: Int = Color.BLACK
+
+    @Px
+    private var mTextSize: Int? = null
+
+    @Px
+    private var mLineSpacingExtra: Int = 0
+    private var mLineSpacingMultiplier: Float = 1.0F
+
+    @Px
+    private var mVerticalSpacing: Float = 0F
+
+    @Px
+    private var mImageCorners: Float = 0F
+
+    private var mImageWidth: Int = IMAGE_WIDTH_MATCH
+
     private var mAdapter: UBBContentAdapter? = null
     private val mUBBContentList = mutableListOf<UBBContentBean>()
 
     private var mJob: Job? = null
 
-    constructor(context: Context?) : this(context, null)
-    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        initAttrs(context, attrs)
+    }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     constructor(
-        context: Context?,
+        context: Context,
         attrs: AttributeSet?,
         defStyleAttr: Int,
         defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes)
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+        initAttrs(context, attrs)
+    }
 
     init {
         orientation = VERTICAL
         setAdapter(UBB.getContentAdapter())
+    }
+
+    private fun initAttrs(context: Context, attrs: AttributeSet?) {
+        attrs ?: return
+        val array = TypedArrayUtils.obtainAttributes(
+            resources,
+            context.theme,
+            attrs,
+            R.styleable.UBBContentView
+        )
+        if (array.hasValue(R.styleable.UBBContentView_android_textColor)) {
+            mTextColor = array.getColor(R.styleable.UBBContentView_android_textColor, Color.BLACK)
+        }
+        if (array.hasValue(R.styleable.UBBContentView_android_textSize)) {
+            mTextSize = array.getDimensionPixelSize(
+                R.styleable.UBBContentView_android_textSize, 0
+            )
+        }
+        if (array.hasValue(R.styleable.UBBContentView_android_lineSpacingExtra)) {
+            mLineSpacingExtra = array.getDimensionPixelSize(
+                R.styleable.UBBContentView_android_lineSpacingExtra, mLineSpacingExtra
+            )
+        }
+        if (array.hasValue(R.styleable.UBBContentView_android_lineSpacingMultiplier)) {
+            mLineSpacingMultiplier = array.getFloat(
+                R.styleable.UBBContentView_android_lineSpacingMultiplier, mLineSpacingMultiplier
+            )
+        }
+        if (array.hasValue(R.styleable.UBBContentView_android_verticalSpacing)) {
+            mVerticalSpacing = array.getDimension(
+                R.styleable.UBBContentView_android_verticalSpacing, mVerticalSpacing
+            )
+        }
+        if (array.hasValue(R.styleable.UBBContentView_imageWidth)) {
+            val value = TypedValue()
+            array.getValue(R.styleable.UBBContentView_imageWidth, value)
+            mImageWidth = if (value.data == IMAGE_WIDTH_MATCH || value.data == IMAGE_WIDTH_WRAP) {
+                value.data
+            } else if (value.type == TypedValue.TYPE_DIMENSION) {
+                value.getDimension(context.resources.displayMetrics).roundToInt()
+            } else {
+                IMAGE_WIDTH_MATCH
+            }
+        }
+        if (array.hasValue(R.styleable.UBBContentView_imageCorners)) {
+            mImageCorners = array.getDimension(
+                R.styleable.UBBContentView_imageCorners, mImageCorners
+            )
+        }
     }
 
     fun setAdapter(adapter: UBBContentAdapter) {
